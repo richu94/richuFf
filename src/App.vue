b47adcbbb40699a0de6ad1f5fb03a6b02d340048
richu <1691401076@qq.com>
2024-05-04 18:03:42 +0800

    new-project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!-- {{ message}} -->
+    <element-view></element-view>
+  </div>
+</template>
+
+<script>
+
+import ElementView from './views/element/ElementView.vue'
+export default {
+  components: {ElementView},
+  data: function(){
+    return {
+      message:"Hello Vue"
+    }
+  },
+  method:{
+
+  }
+}
+</script>
+
+<style>
+
+</style>
